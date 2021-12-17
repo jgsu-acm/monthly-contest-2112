85b017fd2cd11e9025ed212e626e05b9f0e34e26
AgOH <stagoh@qq.com>
2021-12-17 15:14:41 +0800

    feat: add A(pro, sol, std)

--- std/A.cpp
+++ std/A.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
+const int maxn = 1e5+5;
+const int mod = 998244353;
+typedef long long ll;
+int a[maxn];
+ll qpow(ll x,ll p,ll mod)
+{
+    ll res=1;
+    for(;p;p>>=1,x=x*x%mod)
+        if(p&1) res=res*x%mod;
+    return res;
+}
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b<<endl;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) cin>>a[i];
+    ll p=1, q=0;
+    for(int i=n-1;i>=0;i--)
+    {
+        swap(p,q);
+        p += a[i]*q;
+        p %= mod;
+    }
+    cout<<(p*qpow(q, mod-2, mod)%mod+mod)%mod<<endl;
     return 0;
 }
