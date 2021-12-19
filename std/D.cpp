280d98c8b0c5b2d5a934cb8afe9872fbd2495573
AgOH <stagoh@qq.com>
2021-12-19 13:33:30 +0800

    feat: add D(std:comment)

--- std/D.cpp
+++ std/D.cpp
@@ -8,9 +8,9 @@ int main()
     cin>>W>>n;
     for(int i=1;i<=n;i++)
         cin>>v[i];
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++)       // 完全背包
         for(int j=v[i];j<=W;j++)
-            dp[j]=max(dp[j-v[i]]+v[i],dp[j]);
-    cout<<W-dp[W]<<endl;
+            dp[j]=max(dp[j-v[i]]+v[i],dp[j]);   // 重量与价值相等
+    cout<<W-dp[W]<<endl;    // 注意是问剩下多少
     return 0;
 }
